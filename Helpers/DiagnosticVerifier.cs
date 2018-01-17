283cd295c867cdd733c578387d115093ee2646d8
Anthony Weems <amlweems@gmail.com>
2018-01-17 00:28:26 -0600

    remove extraneous output

--- Helpers/DiagnosticVerifier.cs
+++ Helpers/DiagnosticVerifier.cs
@@ -114,41 +114,6 @@ namespace RoslynSecurityGuard.Helpers
             for (int i = 0; i < diagnostics.Length; ++i)
             {
                 builder.AppendLine(diagnostics[i].ToString());
-
-                var analyzerType = analyzer.GetType();
-                var rules = analyzer.SupportedDiagnostics;
-
-                foreach (var rule in rules)
-                {
-                    if (rule != null && rule.Id == diagnostics[i].Id)
-                    {
-                        var location = diagnostics[i].Location;
-                        if (location == Location.None)
-                        {
-                            builder.AppendFormat("GetGlobalResult({0}.{1})", analyzerType.Name, rule.Id);
-                        }
-                        else
-                        {
-                            string resultMethodName = diagnostics[i].Location.SourceTree.FilePath.EndsWith(".cs") ? "GetCSharpResultAt" : "GetBasicResultAt";
-                            var linePosition = diagnostics[i].Location.GetLineSpan().StartLinePosition;
-
-                            builder.AppendFormat("{0}({1}, {2}, {3}.{4})",
-                                resultMethodName,
-                                linePosition.Line + 1,
-                                linePosition.Character + 1,
-                                analyzerType.Name,
-                                rule.Id);
-                        }
-
-                        if (i != diagnostics.Length - 1)
-                        {
-                            builder.Append(',');
-                        }
-
-                        builder.AppendLine();
-                        break;
-                    }
-                }
             }
             return builder.ToString();
         }
